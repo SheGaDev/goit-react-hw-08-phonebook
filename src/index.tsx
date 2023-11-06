62b70174304c83067096002ebe27d3bb48053909
SheGaDev <61838855+SheGaDev@users.noreply.github.com>
2023-11-06 12:43:41 +0200

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,21 @@
 import './global.css';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import App from './components/App';
-import { persistor, store } from './redux/store';
+import { BrowserRouter } from 'react-router-dom';
+import App from 'components/App';
+import { persistor, store } from 'redux/store';
 import { PersistGate } from 'redux-persist/integration/react';
+import AuthProvider from 'providers/AuthProvider';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
-      <App />
-    </PersistGate>
+    <AuthProvider>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter basename='/goit-react-hw-08-phonebook'>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
+    </AuthProvider>
   </Provider>
 );
