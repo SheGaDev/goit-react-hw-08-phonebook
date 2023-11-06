ab5038106cabd6785a1e7937032063c64206d4df
SheGaDev <61838855+SheGaDev@users.noreply.github.com>
2023-11-06 12:43:38 +0200

    Add auth provider

---
+++ src/providers/AuthProvider.tsx
@@ -0,0 +1,21 @@
+import { ReactNode, useEffect } from 'react';
+import { useAuth } from 'redux/selectors';
+import { authorization } from 'redux/slice/auth-slice';
+import { useAppDispatch } from 'redux/store';
+import { authUserThunk } from 'redux/thunk/auth-thunk';
+
+const AuthProvider = ({ children }: { children: ReactNode }) => {
+  const auth = useAuth();
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(authorization());
+    if (auth.isAuth) {
+      dispatch(authUserThunk());
+    }
+  }, []);
+
+  return <>{children}</>;
+};
+
+export default AuthProvider;
