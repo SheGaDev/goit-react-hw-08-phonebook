f32d425d2928cf5cb285e23a7707ad62f070b584
SheGaDev <61838855+SheGaDev@users.noreply.github.com>
2023-11-06 12:40:39 +0200

    Add animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ['./src/**/*.ts', './src/**/*.tsx'],
-    theme: {
-      extend: {},
+  content: ['./src/**/*.ts', './src/**/*.tsx'],
+  theme: {
+    extend: {
+      keyframes: {
+        'translate-down': {
+          from: { top: '-20px', opacity: 0 },
+          to: { top: '35px', opacity: 1 },
+        },
+        'opacity-visible': {
+          from: { opacity: 0 },
+          to: { opacity: 1 },
+        },
+      },
+      animation: {
+        'translate-down': 'translate-down 250ms ease-out',
+        'opacity-visible': 'opacity-visible 0.9s ease-out',
+      },
     },
-    plugins: [],
-  };
+  },
+  plugins: [],
+};
