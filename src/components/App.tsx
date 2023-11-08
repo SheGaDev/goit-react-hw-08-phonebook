3e2440bbb94bf8772abc20ddf4463697af5ff215
SheGaDev <61838855+SheGaDev@users.noreply.github.com>
2023-11-08 13:52:22 +0200

    Update App.tsx

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -4,7 +4,6 @@ import PrivateRoute from 'routes/private-route';
 import PublicRoute from 'routes/public-route';
 
 import Layout from './layout/Layout';
-import { useAuth } from 'redux/selectors';
 
 const Home = lazy(() => import('pages/Home'));
 const Contacts = lazy(() => import('pages/Contacts'));
@@ -12,22 +11,14 @@ const Login = lazy(() => import('pages/Login'));
 const Register = lazy(() => import('pages/Register'));
 
 const App = () => {
-  const { isAuth } = useAuth();
-
   return (
     <Routes>
       <Route path={'/'} element={<Layout />}>
-        {isAuth ? (
-          <>
-            <Route index element={<PrivateRoute children={<Home />} />} />
-            <Route path={'contacts'} element={<PrivateRoute children={<Contacts />} />} />
-          </>
-        ) : (
-          <>
-            <Route path={'login'} element={<PublicRoute children={<Login />} />} />
-            <Route path={'register'} element={<PublicRoute children={<Register />} />} />
-          </>
-        )}
+        <Route index element={<PrivateRoute children={<Home />} />} />
+        <Route path={'contacts'} element={<PrivateRoute children={<Contacts />} />} />
+
+        <Route path={'login'} element={<PublicRoute children={<Login />} />} />
+        <Route path={'register'} element={<PublicRoute children={<Register />} />} />
       </Route>
     </Routes>
   );
