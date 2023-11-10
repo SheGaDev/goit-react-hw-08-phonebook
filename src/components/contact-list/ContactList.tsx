4630102bd299294baefd4cde171ae9d1f853329f
SheGaDev <61838855+SheGaDev@users.noreply.github.com>
2023-11-10 02:13:10 +0200

    relocation loader

--- src/components/contact-list/ContactList.tsx
+++ src/components/contact-list/ContactList.tsx
@@ -4,7 +4,6 @@ import ButtonEdit from 'components/buttons/ButtonEdit';
 import ContactEdit from 'components/forms/ContactEdit';
 import { useFilterItems } from 'redux/selectors';
 import { IContact } from 'types/contacts-types';
-import Loader from 'components/loader/Loader';
 
 const ContactList = () => {
   const contacts = useFilterItems();
@@ -22,35 +21,32 @@ const ContactList = () => {
   };
 
   return (
-    <>
-      {contacts.isLoading && <Loader />}
-      <div className='flex w-[100%] flex-col items-center justify-center rounded border-[1px] border-black p-4'>
-        {openForm.isOpen && <ContactEdit contact={openForm.contact} onClose={closeFormEdit} />}
-        {contacts?.items?.length !== 0 ? (
-          <ul className='flex w-[90%] flex-col gap-3'>
-            {contacts.items.map((contact) => (
-              <li
-                key={contact.id}
-                className='group/item flex w-[100%] animate-translate-down justify-between rounded p-2 shadow hover:bg-slate-100'
-              >
-                <div className='flex flex-col'>
-                  <span>Name: {contact.name}</span>
-                  <span>Number: {contact.number}</span>
-                </div>
-                <div className='group/edit invisible flex animate-[translate-down] items-center gap-3 group-hover/item:visible'>
-                  <ButtonEdit editContact={editContact} contact={contact} />
-                  <ButtonDelete id={contact.id} />
-                </div>
-              </li>
-            ))}
-          </ul>
-        ) : (
-          <span>
-            <b>Empty!</b>
-          </span>
-        )}
-      </div>
-    </>
+    <div className='flex w-[100%] flex-col items-center justify-center rounded border-[1px] border-black p-4'>
+      {openForm.isOpen && <ContactEdit contact={openForm.contact} onClose={closeFormEdit} />}
+      {contacts?.items?.length !== 0 ? (
+        <ul className='flex w-[90%] flex-col gap-3'>
+          {contacts.items.map((contact) => (
+            <li
+              key={contact.id}
+              className='group/item flex w-[100%] animate-translate-down justify-between rounded p-2 shadow hover:bg-slate-100'
+            >
+              <div className='flex flex-col'>
+                <span>Name: {contact.name}</span>
+                <span>Number: {contact.number}</span>
+              </div>
+              <div className='group/edit invisible flex animate-[translate-down] items-center gap-3 group-hover/item:visible'>
+                <ButtonEdit editContact={editContact} contact={contact} />
+                <ButtonDelete id={contact.id} />
+              </div>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <span>
+          <b>Empty!</b>
+        </span>
+      )}
+    </div>
   );
 };
 
--- src/providers/AuthProvider.tsx
+++ src/providers/AuthProvider.tsx
@@ -1,4 +1,5 @@
-import { ReactNode, useEffect } from 'react';
+import Loader from 'components/loader/Loader';
+import { ReactNode, useEffect, useState } from 'react';
 import { useAuth } from 'redux/selectors';
 import { updateContacts } from 'redux/slice/contacts-slice';
 import { setUser } from 'redux/slice/user-slice';
@@ -9,18 +10,26 @@ import { authorizationAxios } from 'services/instance';
 const AuthProvider = ({ children }: { children: ReactNode }) => {
   const dispatch = useAppDispatch();
   const { isAuth, token } = useAuth();
+  const [isLoading, setIsLoading] = useState<boolean>(false);
 
   useEffect(() => {
     authorizationAxios(token);
     if (isAuth) {
+      setIsLoading(true);
       fetchProfile().then(({ name, email, contacts }) => {
         dispatch(setUser({ name, email, contacts: contacts.length }));
         dispatch(updateContacts(contacts));
+        setIsLoading(false);
       });
     }
   }, []);
 
-  return <>{children}</>;
+  return (
+    <>
+      {isLoading && <Loader />}
+      {children}
+    </>
+  );
 };
 
 export default AuthProvider;
